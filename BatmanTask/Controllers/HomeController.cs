b87f828fb127055ef23e0fc318d845967c2096de
Grethen <gretttaaa@abv.bg>
2022-01-09 19:09:37 +0200

    Add project files.

---
+++ BatmanTask/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BatmanTask.Models;
+using BatmanTask.Services;
+
+namespace BatmanTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private HelpService service = new HelpService();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int countOfNumbers, int startNumber)
+        {
+            if (countOfNumbers > 0)
+            {
+                try
+                {
+                    int result = service.CalculateSum(countOfNumbers, startNumber);                    
+                    ViewBag.result = string.Format("The mystery solution is: {0} ", result);                       
+                  
+                }
+                catch (Exception e)
+                {
+                    _logger.LogInformation(e.Message);
+                    ViewBag.result = "There was an error while performing the request.";                  
+                }
+            }
+            else
+            {
+                ViewBag.result = "The count of numbers cannot be less than 1!";               
+            }
+            return View();
+        }      
+    }
+}
---
+++ BatmanTask/Services/HelpService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatmanTask.Services
+{
+    public class HelpService
+    {
+        /// <summary>
+        /// This is the entry method responding to the controller.
+        /// </summary>
+        /// <param name="countOfNumbers"></param>
+        /// <param name="startNumber"></param>
+        /// <returns></returns>
+        public int CalculateSum(int countOfNumbers, int startNumber)
+        {
+            List<int> pairSum = new List<int>();
+            List<int> pairSumTemp = new List<int>();
+
+            if (countOfNumbers == 1)
+            {
+                return startNumber;
+            }
+
+            InitializePairSum(countOfNumbers, startNumber, pairSum);
+            pairSum = CalculateFinalSum(pairSum);
+            return pairSum[0];
+        }
+
+        /// <summary>
+        /// This method initializes the first calculated row containing pairs from the initial input.
+        /// </summary>
+        /// <param name="countOfNumbers"></param>
+        /// <param name="startNumber"></param>
+        /// <param name="pairSum"></param>
+        private static void InitializePairSum(int countOfNumbers, int startNumber, List<int> pairSum)
+        {
+            for (int i = 0; i < countOfNumbers - 1; i++)
+            {
+                int currentNum = startNumber + i;
+                int sumPair = currentNum + (currentNum + 1);
+                pairSum.Add(sumPair);
+            }
+        }
+
+        /// <summary>
+        /// This method produces the final sum.
+        /// </summary>
+        /// <param name="pairSum"></param>
+        /// <returns></returns>
+        private static List<int> CalculateFinalSum(List<int> pairSum)
+        {
+            List<int> pairSumTemp = new List<int>();
+
+            while (pairSum.Count > 1)
+            {
+                for (int i = 0; i < pairSum.Count - 1; i++)
+                {
+                    pairSumTemp.Add(pairSum[i] + pairSum[i + 1]);
+                }
+                pairSum = pairSumTemp.ToList();
+                pairSumTemp.Clear();
+            }
+
+            return pairSum;
+        }
+    }
+}
